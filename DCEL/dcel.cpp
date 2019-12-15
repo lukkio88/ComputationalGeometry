19a0f1ad1f70556072df7e003668c7c5248ee28b
lukkio88 <luca.gagliano.1988@gmail.com>
2019-12-15 17:11:58 +0000

    weak dcel in its separate sources

--- DCEL/dcel.cpp
+++ DCEL/dcel.cpp
@@ -1,57 +1,6 @@
 #include <dcel.h>
 #include <line_seg_intersection.h>
 
-using std::cout;
-using std::endl;
-
-VertexIter WeakDCEL::addVertex(Point coords) {
-	int idx = mVertex.size();
-	return mVertex.insert(mVertex.end(), Vertex{ idx, coords,heEnd() });
-}
-
-HalfEdgeIter WeakDCEL::addHalfEdge(VertexIter vertexStart, VertexIter vertexEnd) {
-	HalfEdgeIter inner = mHalfEdge.insert(mHalfEdge.end(), HalfEdge{vertexStart,heEnd(),heEnd(),heEnd(),fEnd()});
-	HalfEdgeIter outer = mHalfEdge.insert(mHalfEdge.end(), HalfEdge{vertexEnd, heEnd(),heEnd(),heEnd(),fEnd()});
-	inner->twin = outer;
-	outer->twin = inner;
-	return inner;
-}
-
-VertexIter WeakDCEL::splitEdge(VertexIter vertexHandle, HalfEdgeIter innerHalfEdge)
-{
-	vertexHandle->incident = innerHalfEdge;
-	HalfEdgeIter outerHalfEdge = innerHalfEdge->twin;
-	HalfEdgeIter innerNext = innerHalfEdge->next;
-	HalfEdgeIter outerPrev = outerHalfEdge->prev;
-	VertexIter startVertex = innerHalfEdge->origin;
-	VertexIter endVertex = outerHalfEdge->origin;
-	//FaceIter innerFace = innerHalfEdge->incident;
-	//FaceIter outerFace = outerHalfEdge->incident;
-
-	HalfEdgeIter newInner = mHalfEdge.insert(heEnd(), HalfEdge{});
-	HalfEdgeIter newOuter = mHalfEdge.insert(heEnd(), HalfEdge{});
-
-	newInner->origin = vertexHandle;
-	newInner->prev = innerHalfEdge;
-	newInner->next = innerNext;
-	newInner->twin = newOuter;
-	//newInner->incident = innerFace;
-
-	newOuter->origin = endVertex;
-	newOuter->prev = outerPrev;
-	newOuter->next = outerHalfEdge;
-	newOuter->twin = newInner;
-	//newOuter->incident = outerFace;
-
-	outerPrev->next = newOuter;
-	innerNext->prev = newInner;
-	innerHalfEdge->next = newInner;
-	outerHalfEdge->prev = newOuter;
-
-	return vertexHandle;
-
-}
-
 VertexIter DCEL::splitEdge(VertexIter vertexHandle, HalfEdgeIter innerHalfEdge)
 {
 
